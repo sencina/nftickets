a8fc70cc8602c0d8d44c84b3d274a2464280f534
Santiago Encina <santiago.encina@ing.austral.edu.ar>
2024-08-24 19:03:24 -0300

    hook test

--- packages/nftickets/src/index.ts
+++ packages/nftickets/src/index.ts
@@ -1,18 +1,15 @@
-import express, { Request, Response , Application } from 'express';
+import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
-import { PORT } from './modules/env';
+import { PORT } from './env';
 
 dotenv.config();
 
 const app: Application = express();
-const port = PORT
+const port = PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
-
-
-
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
