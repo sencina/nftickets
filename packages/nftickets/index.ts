ac5bc68275779e5c56e5a917e5e3d1877f0710c8
Santiago Encina <santiago.encina@ing.austral.edu.ar>
2024-08-24 18:11:24 -0300

    env module

--- packages/nftickets/index.ts
+++ packages/nftickets/index.ts
@@ -1,11 +1,11 @@
-import express, { Express, Request, Response , Application } from 'express';
+import express, { Request, Response , Application } from 'express';
 import dotenv from 'dotenv';
+import { PORT } from './modules/env';
 
-//For env File 
 dotenv.config();
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const port = PORT
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
