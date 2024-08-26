f7fe7dbfd5c0cf630d4052ad3831b6f4083314ea
Santiago Encina <santiago.encina@ing.austral.edu.ar>
2024-08-26 11:41:40 -0300

    nft and script with image

--- packages/web3/ignition/modules/MyNFT.ts
+++ packages/web3/ignition/modules/MyNFT.ts
@@ -2,11 +2,13 @@ import { ethers } from 'hardhat';
 
 const main = async () => {
   const MyContract = await ethers.getContractFactory('MyNFT');
-  const myContract = await MyContract.deploy();
+  const metadataHash = 'bafkreiccparztqw5li6osq6ikcvllijutaoaiih6lmthiabnwnkgy66mnq';
+  const myContract = await MyContract.deploy(`ipfs://${metadataHash}`);
   await myContract.waitForDeployment();
   const address = await myContract.getAddress();
   console.log('MyContract deployed to:', address);
 };
+
 main()
   .then(() => process.exit(0))
   .catch((error) => {
