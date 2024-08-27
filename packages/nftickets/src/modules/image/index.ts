f476d8c162faad1c8140dda9316444cbb3535b77
Santiago Encina <santiago.encina@ing.austral.edu.ar>
2024-08-27 20:19:01 -0300

    qr + image generation

--- packages/nftickets/src/modules/image/index.ts
+++ packages/nftickets/src/modules/image/index.ts
@@ -1,7 +1,8 @@
-import canvas, { createCanvas } from 'canvas';
+import canvas, { createCanvas, loadImage } from 'canvas';
 import { IMAGE_HEIGHT, IMAGE_WIDTH, LAYER_DIR, OUTPUT_DIR } from './utils/constants';
 import { writeFileSync } from 'fs';
 import path from 'path';
+import { toDataURL } from 'qrcode';
 
 export const drawImage = async (nftID: string) => {
   const canvas = createCanvas(IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -15,6 +16,10 @@ export const drawImage = async (nftID: string) => {
 
   await Promise.all(layers);
 
+  const qrCode = await generateQRCode();
+  const qr = await loadImage(qrCode);
+  ctx.drawImage(qr, 75, 50, IMAGE_WIDTH - 150, IMAGE_HEIGHT - 150);
+
   ctx.fillStyle = '#ffffff';
   ctx.font = '20px Nunito, sans-serif';
   ctx.fillText(`Encigoool`, 120, 460);
@@ -22,6 +27,11 @@ export const drawImage = async (nftID: string) => {
   const buffer = canvas.toBuffer('image/png');
   const p = path.resolve(__dirname, `${OUTPUT_DIR}/test.png`);
   writeFileSync(p, buffer);
+};
+
+const generateQRCode = async () => {
+  const qrCode = await toDataURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  return qrCode;
 };
 
 const addLayer = async (traitType: string, val: string, ctx: canvas.CanvasRenderingContext2D) => {
